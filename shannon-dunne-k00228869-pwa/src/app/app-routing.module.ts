9dd842ec497fd977db9b95d287ff80f654d72b79
Shannon Dunne <k00228869@student.lit.ie>
2021-02-02 13:46:11 +0000

    addint component routes

--- shannon-dunne-k00228869-pwa/src/app/app-routing.module.ts
+++ shannon-dunne-k00228869-pwa/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'sign-up', component: SignUpComponent},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],

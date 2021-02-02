d465af2d7c491946c749ca38c7cf5c575cee66a0
Shannon Dunne <k00228869@student.lit.ie>
2021-02-02 11:34:34 +0000

    Creating User interface

---
+++ shannon-dunne-k00228869-pwa/src/app/i-user.ts
@@ -0,0 +1,55 @@
+export interface IUser {
+    user: {
+        uid: string;
+        email: string;
+        password: string;
+        firstName: string;
+        lastName: string;
+
+        // roles: {
+        //     subscriber?: boolean;
+        //     editor?: boolean;
+        //     admin?: boolean;
+        // };
+    };
+    slides?: {
+        image: string,
+    }[];
+    business?: {
+        id: number,
+        profileCreated: boolean;
+        businessName: string,
+        businessDescription: string,
+        eircode: string,
+        county: string,
+        phoneNumber: string,
+        businessType: string,
+        reminderMessage: string,
+        isMobile: boolean,
+        cancellationPolicy?: string,
+        profileImg?: string
+    };
+    reviews?: {
+        date?: number;
+        id: number;
+        rating: number;
+        name: string;
+        description: string;
+        business: string;
+        comment: string; }[];
+    employees?: {
+        id: number;
+        firstName: string;
+        lastName: string;
+        employeeDescription: string;
+        employeeServices: string;
+        emloyeeImg: string;
+        // days?: string;
+        hours?: string; }[];
+    services?: {
+        id: number;
+        serviceName: string;
+        serviceDescription: string;
+        servicePrice: number;
+        duration: number; }[];
+}

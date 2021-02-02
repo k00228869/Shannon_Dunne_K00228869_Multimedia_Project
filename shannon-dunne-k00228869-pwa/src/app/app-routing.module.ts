b1b6062015be043fd8050f5c78a83c82bbb3a5b4
Shannon Dunne <k00228869@student.lit.ie>
2021-02-02 14:24:26 +0000

    landing page route

--- shannon-dunne-k00228869-pwa/src/app/app-routing.module.ts
+++ shannon-dunne-k00228869-pwa/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 const routes: Routes = [
   {path: 'sign-up', component: SignUpComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: LandingPageComponent}
 ];
 
 @NgModule({

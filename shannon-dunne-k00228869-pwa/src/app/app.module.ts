20bea02ec328789efa4c89ba8579a4fad6dccce8
Shannon Dunne <k00228869@student.lit.ie>
2021-02-02 12:09:43 +0000

    Login html template

--- shannon-dunne-k00228869-pwa/src/app/app.module.ts
+++ shannon-dunne-k00228869-pwa/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,

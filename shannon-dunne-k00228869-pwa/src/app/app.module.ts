a16911b560c9a65cfb2bd52ef7c4b566309918a4
Shannon Dunne <k00228869@student.lit.ie>
2021-02-02 13:23:01 +0000

    adding form group to login.ts

--- shannon-dunne-k00228869-pwa/src/app/app.module.ts
+++ shannon-dunne-k00228869-pwa/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
-
+import { IUser } from './i-user';
 @NgModule({
   declarations: [
     AppComponent,
--- shannon-dunne-k00228869-pwa/src/app/components/login/login.component.ts
+++ shannon-dunne-k00228869-pwa/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+loginForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private login: FormBuilder
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.loginForm = this.login.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [Validators.minLength(6), Validators.maxLength(25)]
+    })
   }
+
+
 
 }
